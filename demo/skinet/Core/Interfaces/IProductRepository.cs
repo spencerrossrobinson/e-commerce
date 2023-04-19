f519f5a59de89ccf214306051610138ad7e5b3a2
Spencer Robinson <spencerwarmouth@gmail.com>
2023-04-19 15:52:56 -0400

    Product Type and Brand HTTPGet

--- demo/skinet/Core/Interfaces/IProductRepository.cs
+++ demo/skinet/Core/Interfaces/IProductRepository.cs
@@ -9,5 +9,7 @@ namespace Core.Interfaces
     {
         Task<Product> GetProductByIdAsync(int id);
         Task<IReadOnlyList<Product>> GetProductsAsync();
+        Task<IReadOnlyList<ProductBrand>> GetProductBrandsAsync();
+        Task<IReadOnlyList<ProductType>> GetProductTypesAsync();
     }
 }
