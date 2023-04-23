8006d8cb09c9376a9694ba2c3731a1d4f5f1f007
Spencer Robinson <spencerwarmouth@gmail.com>
2023-04-23 11:47:32 -0400

    Integrating Specification method in the controller

---
+++ demo/skinet/Core/Specifications/ProductsWithTypesAndBrandsSpecification.cs
@@ -0,0 +1,14 @@
+
+using Core.Entities;
+
+namespace Core.Specifications
+{
+    public class ProductsWithTypesAndBrandsSpecification : BaseSpecification<Product>
+    {
+        public ProductsWithTypesAndBrandsSpecification()
+        {
+            AddInclude(x => x.ProductType);
+            AddInclude(x => x.ProductBrand);
+        }
+    }
+}
