53f822d946fe00bb3d38669a9057c06453b81522
Spencer Robinson <spencerwarmouth@gmail.com>
2023-04-26 17:34:48 -0400

    cleaning up start up

--- demo/skinet/API/Controllers/ProductsController.cs
+++ demo/skinet/API/Controllers/ProductsController.cs
@@ -1,5 +1,6 @@
 
 using API.Dtos;
+using API.Errors;
 using AutoMapper;
 using Core.Entities;
 using Core.Interfaces;
@@ -44,10 +45,14 @@ namespace API.Controllers
         }
 
         [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status404NotFound)]
         public async Task<ActionResult<ProductToReturnDto>> GetProduct(int id)
         {
             var spec = new ProductsWithTypesAndBrandsSpecification(id);
            var product = await _productsRepo.GetEntityWithSpec(spec);
+
+           if (product == null) return NotFound(new ApiResponse(404));
 
            return _mapper.Map<Product, ProductToReturnDto>(product);
         }
